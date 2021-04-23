f8dafa9baff34be916d8080801337fffec0e486b
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-04-23 19:34:16 +0530

    add static file public directory

--- src/file/index.ts
+++ src/file/index.ts
@@ -1,24 +1,6 @@
-import {Router} from "express";
-//@ts-ignore
-//import s3Proxy from 's3-proxy'
+import express, {Router} from "express";
 
-export const rFile = Router();
-
-// const AWS_ACCESS_KEY =  process.env.AWS_SECRET_ACCESS_KEY
-// const AWS_ACCESS_ID =  process.env.AWS_ACCESS_KEY_ID
-// // const AWS_REGION =  process.env.AWS_REGION
-// const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
-// 
-// rFile.get('/*', s3Proxy({
-//     bucket: AWS_BUCKET_NAME,
-//     // prefix: 'optional_s3_path_prefix',
-//     accessKeyId: AWS_ACCESS_ID,
-//     secretAccessKey: AWS_ACCESS_KEY,
-//     overrideCacheControl: 'max-age=100000',
-//     // defaultKey: 'index.html'
-// }));
-
-rFile.get('/', (_, res) => res.send('File module is not configured!'))
+const STATIC_FILE_DIRECTORY = "../../public"
 
 // Router
-export default rFile
+export default express.static(STATIC_FILE_DIRECTORY)
